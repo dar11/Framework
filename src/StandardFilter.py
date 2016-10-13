86ea3912f04fe6a77871b9e0dbc69850b7e68cff
Daniel Arnsberger <daniel.arnsberger@googlemail.com>
2016-10-13 16:27:47 +0200

    Minor.

--- src/StandardFilter.py
+++ src/StandardFilter.py
@@ -102,4 +102,39 @@ class ConvexHull(AbstractFilter):
     
     def getParameters(self):
         return ["No Parameters"]
+    
+class MaxInscribed(AbstractFilter):
+    
+    def __init__(self):
+        self.name = "MaxInscribed"
+        self.max_inscribed_center = (0,0)
+        self.max_inscribed_radius = 0
         
+    def execute(self, image, orig):
+        temp_cont = image.copy()
+        cont_image, contours, hierarchy = cv2.findContours(temp_cont, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            return image, orig
+        contour = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(contour)
+        rows, cols = image.shape[:2]
+        distance = -1
+        maxDistance = -1
+        center = None
+        if (cv2.contourArea(contour) > 5000):
+            for i in xrange(x, x+w, 10):
+                for j in xrange(y, y+h, 10):
+                    distance = cv2.pointPolygonTest(contour, (i,j), True)
+                    if distance > maxDistance:
+                        maxDistance = distance
+                        center = (i,j)
+                        
+            maxDistance = int(maxDistance)
+            self.max_inscribed_center = center
+            self.max_inscribed_radius = maxDistance
+            
+        cv2.circle(orig, self.max_inscribed_center, self.max_inscribed_radius, (0,0,0), 1)
+        return image, orig
+    
+    def getParameters(self):
+        return ["Center of Circle: " + str(self.max_inscribed_center), "Radius of Circle: " + str(self.max_inscribed_radius)]
